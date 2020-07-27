4f20a4d517cc63082a442cbe83190b988b9345b5
vishal sharma <vishalsh533@gmail.com>
2020-07-27 09:19:16 +0530

    updated path

--- freshlybuiltimagejaano/__init__.py
+++ freshlybuiltimagejaano/__init__.py
@@ -1,3 +1,3 @@
 # Class import
 from freshlybuiltimagejaano.test_generator import image_description
-from .downloader import model_downloader
+from freshlybuiltimagejaano.downloader import model_downloader
