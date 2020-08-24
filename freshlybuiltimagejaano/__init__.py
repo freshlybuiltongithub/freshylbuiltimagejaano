08197129ebd54feac32101fa57c84855da86e35b
KapilBansal320 <kapilbansal.gbpecdelhi@gmail.com>
2020-08-24 14:11:56 +0530

    Code Reformatting

--- freshlybuiltimagejaano/__init__.py
+++ freshlybuiltimagejaano/__init__.py
@@ -1,4 +1,4 @@
 # Class import
-from freshlybuiltimagejaano.test_generator import image_description
 from freshlybuiltimagejaano.downloader import model_downloader
 from freshlybuiltimagejaano.get_token import token
+from freshlybuiltimagejaano.test_generator import image_description
--- freshlybuiltimagejaano/get_token.py
+++ freshlybuiltimagejaano/get_token.py
@@ -1,8 +1,9 @@
-from requests import get
-from os import mkdir,path,remove
-from tqdm import tqdm
 from hashlib import md5
-from colorama import init,deinit,Fore
+from os import mkdir, path, remove
+
+from colorama import Fore, deinit, init
+from requests import get
+from tqdm import tqdm
 
 """
 freshlybuiltimagejaanolibrary tokenizer file
@@ -26,78 +27,83 @@ function token_check : token_check implements md5 hash checksum for checking the
 
 """
 
+
 class token:
-    status_code=0
-    token_checksum='b8bc863c36b0c5ea638fdb75ea86e1bc'
-    curr_dir=path.dirname(path.realpath(__file__))
+    status_code = 0
+    token_checksum = 'b8bc863c36b0c5ea638fdb75ea86e1bc'
+    curr_dir = path.dirname(path.realpath(__file__))
 
     def __init__(self):
-        self.check_token(self.status_code,self.curr_dir)
+        self.check_token(self.status_code, self.curr_dir)
 
-    def check_token(self,status_code,curr_dir):
-        if path.isfile(curr_dir+"/tokenizer.p"):
+    def check_token(self, status_code, curr_dir):
+        if path.isfile(curr_dir + "/tokenizer.p"):
             print("tokenizer found")
-            self.token_check(self.token_checksum)  
+            self.token_check(self.token_checksum)
         else:
-            print("downloading tokenizer")    
+            print("downloading tokenizer")
             self.get_token()
 
     def get_token(self):
-        token_url= "https://raw.githubusercontent.com/freshlybuiltongithub/freshylbuiltimagejaano/master/freshlybuiltimagejaano/tokenizer.p"
+        token_url = "https://raw.githubusercontent.com/freshlybuiltongithub/freshylbuiltimagejaano/master/freshlybuiltimagejaano/tokenizer.p"
         response = get(token_url, stream=True)
         try:
-            response.raise_for_status()   
+            response.raise_for_status()
         except response.exceptions.HTTPError as errh:
-            print ("Http Error:",errh)
-            self.status_code=2003
+            print("Http Error:", errh)
+            self.status_code = 2003
             return self.status_code
         except response.exceptions.ConnectionError as errc:
-            print ("Error Connecting:",errc)
-            self.status_code=2004
+            print("Error Connecting:", errc)
+            self.status_code = 2004
             return self.status_code
         except response.exceptions.Timeout as errt:
-            print ("Timeout Error:",errt)
-            self.status_code=2005
+            print("Timeout Error:", errt)
+            self.status_code = 2005
             return self.status_code
         except response.exceptions.RequestException as err:
-            print ("OOps: Something Else",err)
-            self.status_code=2006
+            print("OOps: Something Else", err)
+            self.status_code = 2006
             return self.status_code
-        with open(self.curr_dir+"/"+"tokenizer.p", "wb") as f:
+        with open(self.curr_dir + "/" + "tokenizer.p", "wb") as f:
             total_length = int(response.headers.get('content-length'))
             if total_length is None:
                 f.write(response.content)
             else:
-                chunk_size=512
-                for data in tqdm(iterable = response.iter_content(chunk_size),total=total_length/chunk_size, unit = 'KB'):
+                chunk_size = 512
+                for data in tqdm(
+                    iterable=response.iter_content(chunk_size),
+                    total=total_length / chunk_size,
+                    unit='KB',
+                ):
                     try:
                         f.write(data)
                     except:
                         pass
-        self.status_code=2002
-        return self.status_code             
-    
-    def token_check(self,token_checksum):
-        model_checksum="tokenizer.p"
-        md5_hash=md5()
-        model_handler=open(model_checksum,"rb").read()
+        self.status_code = 2002
+        return self.status_code
+
+    def token_check(self, token_checksum):
+        model_checksum = "tokenizer.p"
+        md5_hash = md5()
+        model_handler = open(model_checksum, "rb").read()
         md5_hash.update(model_handler)
-        hash_code=md5_hash.hexdigest()
-        if hash_code==token_checksum:
+        hash_code = md5_hash.hexdigest()
+        if hash_code == token_checksum:
             init(autoreset=True)
             print("tokenizer signature matched")
             deinit()
-            self.status_code=2000
+            self.status_code = 2000
             return self.status_code
 
         else:
             init(autoreset=True)
             print("tokenizer signature mismatched")
             deinit()
-            self.status_code=2001
+            self.status_code = 2001
             print("restarting download")
-            remove(self.curr_dir+"/"+"tokenizer.p")
+            remove(self.curr_dir + "/" + "tokenizer.p")
             self.get_token()
 
 
-#print(token().status_code)
+# print(token().status_code)
